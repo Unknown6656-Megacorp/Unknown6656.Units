8482980e105583d47bae02f5e6dffa63eb39aabe
Unknown6656 <unknown84@gmx.de>
2023-12-19 21:03:27 +0100

    began adding units of length and time

---
+++ Unknown6656.Units/Length.cs
@@ -0,0 +1,84 @@
+﻿using System.Numerics;
+
+namespace Unknown6656.Units.Length;
+
+
+public abstract class Length<TScalar>
+    : Quantity<Length<TScalar>, TScalar>
+    where TScalar : INumber<TScalar>
+{
+}
+
+public abstract class Surface<TScalar>
+    : Quantity<Surface<TScalar>, TScalar>
+    where TScalar : INumber<TScalar>
+{
+}
+
+public abstract class Volume<TScalar>
+    : Quantity<Volume<TScalar>, TScalar>
+    where TScalar : INumber<TScalar>
+{
+}
+
+// TODO:
+// - inch
+// - mile
+// - nautical mile
+// - light year
+// - parsec
+// - astronomical unit
+// - league
+// - fathom
+// - rod
+// - chain
+// - furlong
+// - from KM, CM, MM, ...
+
+
+
+public record Meters(scalar Value)
+    : Length<scalar>.BaseUnit<Meters>(Value)
+    , IBaseUnit<Meters, scalar>
+{
+    public static string UnitSymbol { get; } = "m";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Metric;
+}
+
+public record Yards(scalar Value)
+    : Length<scalar>.AffineUnit<Yards, Meters>(Value)
+    , Length<scalar>.IAffineUnit
+    , IUnit<Yards, Meters, scalar>
+{
+    public static string UnitSymbol { get; } = "yd";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Imperial;
+
+    public static scalar ScalingFactor { get; } = (scalar)1.0936132983;
+    public static scalar PreScalingOffset { get; } = 0;
+    public static scalar PostScalingOffset { get; } = 0;
+}
+
+public record Feet(scalar Value)
+    : Length<scalar>.AffineUnit<Feet, Meters>(Value)
+    , Length<scalar>.IAffineUnit
+    , IUnit<Feet, Meters, scalar>
+{
+    public static string UnitSymbol { get; } = "ft";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Imperial;
+
+    public static scalar ScalingFactor { get; } = (scalar)3.280839895013123359580052493438320209973753;
+    public static scalar PreScalingOffset { get; } = 0;
+    public static scalar PostScalingOffset { get; } = 0;
+}
+
+
+public record SquareMeters(scalar Value)
+    : Length<scalar>.BaseUnit<SquareMeters>(Value)
+    , IBaseUnit<SquareMeters, scalar>
+{
+    public static string UnitSymbol { get; } = "m²";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Metric;
+}
+
+
+
---
+++ Unknown6656.Units/Time.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace Unknown6656.Units.Time;
+
+
+public class Time<TScalar>
+    : Quantity<Time<TScalar>, TScalar>
+    where TScalar : INumber<TScalar>
+{
+}
+
+public record Seconds(scalar Value)
+    : Time<scalar>.BaseUnit<Seconds>(Value)
+    , IBaseUnit<Seconds, scalar>
+{
+    public static string UnitSymbol { get; } = "s";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Metric;
+}
+
+// - hour
+// - day
+// - week
+// - datetime
+// - from seconds, minutes, hours, days, weeks, months, years, ...
+// - from ticks, ...
+// - planck time
+// - fortnight
+// - cesium frequency
