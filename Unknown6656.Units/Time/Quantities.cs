43df53e04cc04e6058f91a9f54d2bdfcf26aed4b
Unknown6656 <unknown84@gmx.de>
2023-12-27 00:37:10 +0100

    added quantities for time

---
+++ Unknown6656.Units/Time/Quantities.cs
@@ -0,0 +1,5 @@
+﻿namespace Unknown6656.Units.Time;
+
+
+//[QuantityDependency<Length, Area, Meters, SquareMeters, Scalar>]
+public partial record Time(Seconds value) : Quantity<Time, Seconds, Scalar>(value);
