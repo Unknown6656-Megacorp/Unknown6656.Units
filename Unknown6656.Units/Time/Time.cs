ce22efefd56e72f8d22d5c921d8e3e3c0e71713c
Unknown6656 <unknown84@gmx.de>
2023-12-27 00:37:20 +0100

    added basic units for time

---
+++ Unknown6656.Units/Time/Time.cs
@@ -0,0 +1,62 @@
+﻿namespace Unknown6656.Units.Time;
+
+
+#pragma warning disable IDE0004 // Remove Unnecessary Cast
+
+
+[KnownBaseUnit<Time, Seconds, Scalar>]
+public partial record Seconds(Scalar Value)
+    : BaseUnit<Time, Seconds, Scalar>(Value)
+    , IBaseUnit<Seconds, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "s";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Metric;
+}
+
+[KnownUnit<Time, Minutes, Seconds, Scalar>]
+public partial record Minutes(Scalar Value)
+    : Time.AffineUnit<Minutes>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<Minutes, Seconds, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "min";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.NonSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)60;
+}
+
+[KnownUnit<Time, Hours, Seconds, Scalar>]
+public partial record Hours(Scalar Value)
+    : Time.AffineUnit<Hours>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<Hours, Seconds, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "h";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.NonSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)3600;
+}
+
+[KnownUnit<Time, PlanckTime, Seconds, Scalar>]
+public partial record PlanckTime(Scalar Value)
+    : Time.AffineUnit<PlanckTime>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<PlanckTime, Seconds, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "tₚ";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.NonSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)5.39116e-44;
+}
+
+
+// // - Time
+// //      - datetime
+// //      - from seconds, minutes, hours, days, weeks, months, years, ...
+// //      - from ticks, ...
+// //      - planck time
+// //      - fortnight
+// // Frequency
+// //      - hertz
+// //      - cesium frequency
