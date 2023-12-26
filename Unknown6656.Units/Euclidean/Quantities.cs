bc05d23e0d6479f14825943048a7c55bd54cd0da
Unknown6656 <unknown84@gmx.de>
2023-12-26 17:12:15 +0100

    split up eucledian measurement quantities

---
+++ Unknown6656.Units/Euclidean/Quantities.cs
@@ -0,0 +1,10 @@
+﻿namespace Unknown6656.Units.Euclidean;
+
+
+[QuantityDependency<Length, Area, Meters, SquareMeters, Scalar>]
+public partial record Length(Meters value) : Quantity<Length, Meters, Scalar>(value);
+
+[QuantityDependency<Area, Length, Volume, SquareMeters, Meters, CubicMeters, Scalar>]
+public partial record Area(SquareMeters value) : Quantity<Area, SquareMeters, Scalar>(value);
+
+public record Volume(CubicMeters value) : Quantity<Volume, CubicMeters, Scalar>(value);
