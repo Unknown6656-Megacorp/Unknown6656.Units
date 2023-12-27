02925b280a674b7a21ab0a568dff1258a2d02aee
Unknown6656 <unknown84@gmx.de>
2023-12-27 17:02:03 +0100

    added acceleration

---
+++ Unknown6656.Units/Movement/Acceleration.cs
@@ -0,0 +1,12 @@
+﻿namespace Unknown6656.Units.Movement;
+
+
+[KnownBaseUnit<Acceleration, MetersPerSecondSquared, Scalar>]
+public partial record MetersPerSecondSquared(Scalar Value)
+    : BaseUnit<Acceleration, MetersPerSecondSquared, Scalar>(Value)
+    , IBaseUnit<MetersPerSecondSquared, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "m/s²";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Metric;
+}
--- Unknown6656.Units/Movement/Quantities.cs
+++ Unknown6656.Units/Movement/Quantities.cs
@@ -1,8 +1,14 @@
 ﻿using Unknown6656.Units.Euclidean;
-using Unknown6656.Units.Time;
+using Unknown6656.Units.Temporal;
 
 namespace Unknown6656.Units.Movement;
 
 
 [MultiplicativeQuantityRelationship<Speed, Time, Length, MetersPerSecond, Seconds, Meters, Scalar>]
 public partial record Speed(MetersPerSecond value) : Quantity<Speed, MetersPerSecond, Scalar>(value);
+
+[MultiplicativeQuantityRelationship<Acceleration, Time, Speed, MetersPerSecondSquared, Seconds, MetersPerSecond, Scalar>]
+public partial record Acceleration(MetersPerSecondSquared value) : Quantity<Acceleration, MetersPerSecondSquared, Scalar>(value);
+
+// TODO : mass flow rate
+// TODO : volumetric flow rate
