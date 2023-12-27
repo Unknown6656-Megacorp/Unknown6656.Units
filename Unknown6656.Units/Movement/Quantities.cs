5407015567459f2c18f92ddaa3a635ed14a7a5e2
Unknown6656 <unknown84@gmx.de>
2023-12-27 16:54:02 +0100

    added baseunit for speed

---
+++ Unknown6656.Units/Movement/Quantities.cs
@@ -0,0 +1,8 @@
+﻿using Unknown6656.Units.Euclidean;
+using Unknown6656.Units.Time;
+
+namespace Unknown6656.Units.Movement;
+
+
+[MultiplicativeQuantityRelationship<Speed, Time, Length, MetersPerSecond, Seconds, Meters, Scalar>]
+public partial record Speed(MetersPerSecond value) : Quantity<Speed, MetersPerSecond, Scalar>(value);
---
+++ Unknown6656.Units/Movement/Speed.cs
@@ -0,0 +1,31 @@
+﻿namespace Unknown6656.Units.Movement;
+
+
+[KnownBaseUnit<Speed, MetersPerSecond, Scalar>]
+public partial record MetersPerSecond(Scalar Value)
+    : BaseUnit<Speed, MetersPerSecond, Scalar>(Value)
+    , IBaseUnit<MetersPerSecond, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "m/s";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Metric;
+}
+
+
+
+// TODO:
+// - speed
+//      - km/h
+//      - mi/h
+//      - m/s
+//      - ft/s
+//      - knots
+//      - mach
+//      - speed of sound
+//      - speed of light
+// - acceleration
+//      - m/s^2
+//      - ft/s^2
+//      - g
+// - propagation speed of waves
+//      - in different media
