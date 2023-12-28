6bbdd4eb702f8d0af131687c0f62385a80cbf95e
Unknown6656 <unknown84@gmx.de>
2023-12-28 02:57:48 +0100

    moved files

---
+++ Unknown6656.Units/Matter/Mass.cs
@@ -0,0 +1,15 @@
+﻿namespace Unknown6656.Units.Matter;
+
+
+
+// TODO:
+// - amount
+//      - mol
+// - mass
+//      - g / kg / mg / ...
+//      - lb
+//      - oz
+//      - stone
+//      - ton
+//      - from g, kg, mg, ...
+//      - from lb, oz, stone, ton, ...
