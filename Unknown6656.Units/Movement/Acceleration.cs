f45ced1f8026c47ae63611c4fcad7f0f31299448
Unknown6656 <unknown84@gmx.de>
2023-12-28 01:29:57 +0100

    added more units

--- Unknown6656.Units/Movement/Acceleration.cs
+++ Unknown6656.Units/Movement/Acceleration.cs
@@ -1,4 +1,4 @@
-namespace Unknown6656.Units.Movement;
+﻿namespace Unknown6656.Units.Movement;
 
 
 [KnownBaseUnit<Acceleration, MetersPerSecondSquared, Scalar>]
@@ -9,4 +9,40 @@ public partial record MetersPerSecondSquared(Scalar Value)
 {
     public static string UnitSymbol { get; } = "m/s²";
     public static UnitSystem UnitSystem { get; } = UnitSystem.MetricSI;
+}
+
+[KnownUnit<Acceleration, FeetPerSecondSquared, MetersPerSecondSquared, Scalar>]
+public partial record FeetPerSecondSquared(Scalar Value)
+    : Acceleration.AffineUnit<FeetPerSecondSquared>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<FeetPerSecondSquared, MetersPerSecondSquared, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "ft/s²";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)3.280839895013123;
+}
+
+[KnownUnit<Acceleration, Gals, MetersPerSecondSquared, Scalar>]
+public partial record Gals(Scalar Value)
+    : Acceleration.AffineUnit<Gals>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<Gals, MetersPerSecondSquared, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "Gal";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.MetricNonSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)1e-2;
+}
+
+[KnownUnit<Acceleration, G, MetersPerSecondSquared, Scalar>]
+public partial record G(Scalar Value)
+    : Acceleration.AffineUnit<G>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<G, MetersPerSecondSquared, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "g₀";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.MetricSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)9.80665;
 }
--- Unknown6656.Units/Movement/Speed.cs
+++ Unknown6656.Units/Movement/Speed.cs
@@ -1,4 +1,4 @@
-namespace Unknown6656.Units.Movement;
+﻿namespace Unknown6656.Units.Movement;
 
 
 [KnownBaseUnit<Speed, MetersPerSecond, Scalar>]
@@ -11,21 +11,71 @@ public partial record MetersPerSecond(Scalar Value)
     public static UnitSystem UnitSystem { get; } = UnitSystem.MetricSI;
 }
 
+[KnownUnit<Speed, KilometersPerHour, MetersPerSecond, Scalar>]
+public partial record KilometersPerHour(Scalar Value)
+    : Speed.AffineUnit<KilometersPerHour>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<KilometersPerHour, MetersPerSecond, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "km/h";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.MetricNonSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)3.6;
+}
+
+[KnownUnit<Speed, MetersPerHour, MetersPerSecond, Scalar>]
+public partial record MetersPerHour(Scalar Value)
+    : Speed.AffineUnit<MetersPerHour>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<MetersPerHour, MetersPerSecond, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "m/h";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.MetricSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)3600;
+}
+
+[KnownUnit<Speed, MilesPerHour, MetersPerSecond, Scalar>]
+public partial record MilesPerHour(Scalar Value)
+    : Speed.AffineUnit<MilesPerHour>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<MilesPerHour, MetersPerSecond, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "mi/h";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)2.2369362920544025;
+}
+
+[KnownUnit<Speed, FeetPerSecond, MetersPerSecond, Scalar>]
+public partial record FeetPerSecond(Scalar Value)
+    : Speed.AffineUnit<FeetPerSecond>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<FeetPerSecond, MetersPerSecond, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "ft/s";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)3.280839895013123;
+}
+
+[KnownUnit<Speed, Knots, MetersPerSecond, Scalar>]
+public partial record Knots(Scalar Value)
+    : Speed.AffineUnit<Knots>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<Knots, MetersPerSecond, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "kn";
+    public static UnitSystem UnitSystem { get; } = UnitSystem.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)1.9438444924406046;
+}
+
 
 
 // TODO:
 // - speed
-//      - km/h
-//      - mi/h
-//      - m/s
-//      - ft/s
-//      - knots
-//      - mach
-//      - speed of sound
-//      - speed of light
-// - acceleration
-//      - m/s^2
-//      - ft/s^2
-//      - g
+//      - mach / speed of sound (per medium)
+//      - speed of light (per medium)
 // - propagation speed of waves
 //      - in different media
