f00bf63ed86c97575b417f46018895691c815f20
Unknown6656 <unknown84@gmx.de>
2023-12-29 12:12:45 +0100

    added poundforce

--- Unknown6656.Units/Movement/Force.cs
+++ Unknown6656.Units/Movement/Force.cs
@@ -9,6 +9,18 @@ public partial record Newton(Scalar Value)
     public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
 }
 
+[KnownUnit<Force, PoundForce, Newton, Scalar>]
+public partial record PoundForce(Scalar Value)
+    : Force.AffineUnit<PoundForce>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<PoundForce, Newton, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "lbf";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)0.2248089430997105;
+}
+
 
 // TODO:
 // - force
