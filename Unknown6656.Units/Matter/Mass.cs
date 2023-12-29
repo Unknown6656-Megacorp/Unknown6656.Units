11ec624f4506b17aebcbbc5e0e01c09bef665c25
Unknown6656 <unknown84@gmx.de>
2023-12-29 18:03:47 +0100

    added more mass units and constants

--- Unknown6656.Units/Matter/Mass.cs
+++ Unknown6656.Units/Matter/Mass.cs
@@ -25,7 +25,7 @@ public partial record MetricTon(Scalar Value)
     , ILinearUnit<Scalar>
 {
     public static string UnitSymbol { get; } = "t";
-    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricNonSI;
     public static Scalar ScalingFactor { get; } = (Scalar)1e-3;
 }
 
@@ -36,21 +36,130 @@ public partial record Dalton(Scalar Value)
 {
     public static string UnitSymbol { get; } = "Da";
     public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
-    public static Scalar ScalingFactor { get; } = (Scalar)1.66053906660e-27;
+    public static Scalar ScalingFactor { get; } = (Scalar)6.022173643e+26;
 }
 
+[KnownUnit<Mass, Pound, Kilogram, Scalar>]
+public partial record Pound(Scalar Value)
+    : Mass.AffineUnit<Pound>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "lb";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)2.2046226218487757;
+}
 
+[KnownUnit<Mass, Ounce, Kilogram, Scalar>]
+public partial record Ounce(Scalar Value)
+    : Mass.AffineUnit<Ounce>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "oz";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)35.27396194958041;
+}
+
+[KnownUnit<Mass, UKTon, Kilogram, Scalar>]
+public partial record UKTon(Scalar Value)
+    : Mass.AffineUnit<UKTon>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "US t";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)9.842065276110606e-4;
+}
+
+[KnownUnit<Mass, USTon, Kilogram, Scalar>]
+public partial record USTon(Scalar Value)
+    : Mass.AffineUnit<USTon>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "s.t.";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)1.1023113109243879e-3;
+}
+
+[KnownUnit<Mass, Grain, Kilogram, Scalar>]
+public partial record Grain(Scalar Value)
+    : Mass.AffineUnit<Grain>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "gr";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)1.543235835294122e4;
+}
+
+[KnownUnit<Mass, Slug, Kilogram, Scalar>]
+public partial record Slug(Scalar Value)
+    : Mass.AffineUnit<Slug>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "slug";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)0.06852176556196146;
+}
+
+[KnownUnit<Mass, UKStone, Kilogram, Scalar>]
+public partial record UKStone(Scalar Value)
+    : Mass.AffineUnit<UKStone>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "UK st";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)0.1574730444177697;
+}
+
+[KnownUnit<Mass, USStone, Kilogram, Scalar>]
+public partial record USStone(Scalar Value)
+    : Mass.AffineUnit<USStone>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "US st";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)0.14285714285714285;
+}
+
+[KnownUnit<Mass, Carat, Kilogram, Scalar>]
+public partial record Carat(Scalar Value)
+    : Mass.AffineUnit<Carat>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "ct";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricNonSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)5e3;
+}
+
+[KnownUnit<Mass, PlanckMass, Kilogram, Scalar>]
+public partial record PlanckMass(Scalar Value)
+    : Mass.AffineUnit<PlanckMass>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "mₚ";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricNonSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)4.59467132297156563388720245813872525405673487755159606404846e7;
+}
+
+[KnownUnit<Mass, SolarMass, Kilogram, Scalar>]
+public partial record SolarMass(Scalar Value)
+    : Mass.AffineUnit<SolarMass>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "M☉";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricNonSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)5.0289921396852856718984948226525921939984007804995800791563e-31;
+}
+
+[KnownUnit<Mass, ElectronVoltMassEquivalent, Kilogram, Scalar>]
+public partial record ElectronVoltMassEquivalent(Scalar Value)
+    : Mass.AffineUnit<ElectronVoltMassEquivalent>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "eV/c²";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)5.60958864983476645299741629583157968944023663372081019062242e35;
+}
 
 
 // TODO:
 // - mass
-//      - lb
-//      - eV/c²
-//      - sl
-//      - planck mass
-//      - solar mass
-//      - oz
-//      - stone
-//      - ton
-//      - from g, kg, mg, ...
-//      - from lb, oz, stone, ton, ...
+//      - from g, kg, mg, lb, oz, stone, ton, ...
--- Unknown6656.Units/Matter/Quantities.cs
+++ Unknown6656.Units/Matter/Quantities.cs
@@ -5,13 +5,30 @@ namespace Unknown6656.Units.Matter;
 
 public partial record Amount(Mol value) : Quantity<Amount, Mol, Scalar>(value);
 
-public partial record Mass(Kilogram value) : Quantity<Mass, Kilogram, Scalar>(value);
+public partial record Mass(Kilogram value)
+    : Quantity<Mass, Kilogram, Scalar>(value)
+{
+    public static ElectronVoltMassEquivalent MassOfElectronNeutrino { get; } = new(0.120);
+    public static ElectronVoltMassEquivalent MassOfMuonNeutrino { get; } = new(0.170);
+    public static ElectronVoltMassEquivalent MassOfTauNeutrino { get; } = new(15.5);
+    public static ElectronVoltMassEquivalent MassOfElectron { get; } = new(511_000);
+    public static ElectronVoltMassEquivalent MassOfMuon { get; } = new(105_700_000);
+    public static ElectronVoltMassEquivalent MassOfTau { get; } = new(1_777_000_000);
+    public static ElectronVoltMassEquivalent MassOfProton { get; } = new(938_000_000);
+    public static ElectronVoltMassEquivalent MassOfNeutron { get; } = new(939_000_000);
+    public static ElectronVoltMassEquivalent MassOfDeuteron { get; } = new(1_875_610_000);
+    public static ElectronVoltMassEquivalent MassOfAlphaParticle { get; } = new(3_727_380_000);
+
+
+    // TODO : atomic masses for entire perdiodic system
+}
 
 
 [MultiplicativeQuantityRelationship<MolarMass, Amount, Mass, GramPerMol, Mol, Kilogram, Scalar>((Scalar)1e-3)]
 public partial record MolarMass(GramPerMol value) : Quantity<MolarMass, GramPerMol, Scalar>(value);
 
-public partial record VolumetricMassDensity(KilogramsPerCubicMeter value) : Quantity<VolumetricMassDensity, KilogramsPerCubicMeter, Scalar>(value);
+[MultiplicativeQuantityRelationship<VolumetricMassDensity, Volume, Mass, KilogramPerCubicMeter, CubicMeter, Kilogram, Scalar>]
+public partial record VolumetricMassDensity(KilogramPerCubicMeter value) : Quantity<VolumetricMassDensity, KilogramPerCubicMeter, Scalar>(value);
 
 [MultiplicativeQuantityRelationship<Force, Area, Pressure, Newton, SquareMeter, Pascal, Scalar>]
 [MultiplicativeQuantityRelationship<Volume, Pressure, Torque, CubicMeter, Pascal, NewtonMeter, Scalar>]
