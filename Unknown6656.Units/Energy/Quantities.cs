3dc9452303d70785da37f895fda7db291b781131
Unknown6656 <unknown84@gmx.de>
2023-12-29 18:03:12 +0100

    added quantites for temperature and kinetic energy

---
+++ Unknown6656.Units/Energy/Quantities.cs
@@ -0,0 +1,11 @@
+﻿namespace Unknown6656.Units.Energy;
+
+
+public partial record Temperature(Kelvin value)
+    : Quantity<Temperature, Kelvin, Scalar>(value)
+{
+
+}
+
+public partial record KineticEnergy(Joule value) : Quantity<KineticEnergy, Joule, Scalar>(value);
+
