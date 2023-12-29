8dcefad073b0a8f2fa472aa65ad8cc2a58f6df38
Unknown6656 <unknown84@gmx.de>
2023-12-29 10:46:33 +0100

    added support for scaling factor in unit relationship

---
+++ Unknown6656.Units/Core/Attributes.cs
@@ -0,0 +1,129 @@
+﻿using System.Numerics;
+using System;
+
+namespace Unknown6656.Units;
+
+
+/// <summary>
+/// Defines the following relationship between two quantities:
+/// <code>
+///     <typeparamref name="TQuantityA"/> * <typeparamref name="TQuantityB"/> = <typeparamref name="TQuantityC"/>
+///     <br/>
+///     <typeparamref name="TBaseUnitA"/> * <typeparamref name="TBaseUnitB"/> = <typeparamref name="TBaseUnitC"/>
+/// </code>
+/// If a value is provided for <paramref name="x"/>, the relationship is defined as follows:
+/// <code>
+///     <typeparamref name="TBaseUnitA"/> * <typeparamref name="TBaseUnitB"/> * <paramref name="x"/> = <typeparamref name="TBaseUnitC"/>
+/// </code>
+/// <para/>
+/// This naturally implies:
+/// <code>
+///     <typeparamref name="TBaseUnitC"/> / <typeparamref name="TBaseUnitA"/> = <typeparamref name="TBaseUnitB"/> * <paramref name="x"/>
+///     <typeparamref name="TBaseUnitC"/> / <typeparamref name="TBaseUnitB"/> = <typeparamref name="TBaseUnitA"/> * <paramref name="x"/>
+/// </code>
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+public class MultiplicativeQuantityRelationship<TQuantityA, TQuantityB, TQuantityC, TBaseUnitA, TBaseUnitB, TBaseUnitC, TScalar>(TScalar? x = default)
+    : Attribute
+    where TQuantityA : Quantity<TQuantityA, TBaseUnitA, TScalar>
+    where TQuantityB : Quantity<TQuantityB, TBaseUnitB, TScalar>
+    where TQuantityC : Quantity<TQuantityC, TBaseUnitC, TScalar>
+    where TBaseUnitA : BaseUnit<TQuantityA, TBaseUnitA, TScalar>
+                     , IBaseUnit<TBaseUnitA, TScalar>
+                     , IUnit
+    where TBaseUnitB : BaseUnit<TQuantityB, TBaseUnitB, TScalar>
+                     , IBaseUnit<TBaseUnitB, TScalar>
+                     , IUnit
+    where TBaseUnitC : BaseUnit<TQuantityC, TBaseUnitC, TScalar>
+                     , IBaseUnit<TBaseUnitC, TScalar>
+                     , IUnit
+    where TScalar : INumber<TScalar>;
+
+/// <summary>
+/// Defines the following relationship between two quantities:
+/// <code>
+///     <typeparamref name="TQuantityA"/> ^ 2 = <typeparamref name="TQuantityB"/>
+///     <br/>
+///     <typeparamref name="TBaseUnitA"/> ^ 2 = <typeparamref name="TBaseUnitB"/>
+/// </code>
+/// If a value is provided for <paramref name="x"/>, the relationship is defined as follows:
+/// <code>
+///     <typeparamref name="TBaseUnitA"/> ^ 2 * <paramref name="x"/> = <typeparamref name="TBaseUnitB"/>
+/// </code>
+/// This naturally implies
+/// <code>
+///     <typeparamref name="TBaseUnitB"/> / <typeparamref name="TBaseUnitA"/> = <typeparamref name="TBaseUnitA"/> * <paramref name="x"/>
+/// </code>
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+public class MultiplicativeQuantityRelationship<TQuantityA, TQuantityB, TBaseUnitA, TBaseUnitB, TScalar>(TScalar? x = default)
+    : MultiplicativeQuantityRelationship<TQuantityA, TQuantityA, TQuantityB, TBaseUnitA, TBaseUnitA, TBaseUnitB, TScalar>(x)
+    where TQuantityA : Quantity<TQuantityA, TBaseUnitA, TScalar>
+    where TQuantityB : Quantity<TQuantityB, TBaseUnitB, TScalar>
+    where TBaseUnitA : BaseUnit<TQuantityA, TBaseUnitA, TScalar>
+                     , IBaseUnit<TBaseUnitA, TScalar>
+                     , IUnit
+    where TBaseUnitB : BaseUnit<TQuantityB, TBaseUnitB, TScalar>
+                     , IBaseUnit<TBaseUnitB, TScalar>
+                     , IUnit
+    where TScalar : INumber<TScalar>;
+
+/// <summary>
+/// Defines the following relationship between two quantities:
+/// <code>
+///     1 / <typeparamref name="TQuantityA"/> = <typeparamref name="TQuantityB"/>
+///     <br/>
+///     1 / <typeparamref name="TBaseUnitA"/> = <typeparamref name="TBaseUnitB"/>
+/// </code>
+/// If a value is provided for <paramref name="x"/>, the relationship is defined as follows:
+/// <code>
+///     <paramref name="x"/> / <typeparamref name="TBaseUnitA"/> = <typeparamref name="TBaseUnitB"/>
+/// </code>
+/// This naturally implies
+/// <code>
+///     <typeparamref name="TBaseUnitB"/> * <typeparamref name="TBaseUnitA"/> = <paramref name="x"/>
+/// </code>
+/// </summary>
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+public class InverseQuantityRelationship<TQuantityA, TQuantityB, TBaseUnitA, TBaseUnitB, TScalar>(TScalar? x = default)
+    : MultiplicativeQuantityRelationship<TQuantityA, TQuantityA, TQuantityB, TBaseUnitA, TBaseUnitA, TBaseUnitB, TScalar>(x)
+    where TQuantityA : Quantity<TQuantityA, TBaseUnitA, TScalar>
+    where TQuantityB : Quantity<TQuantityB, TBaseUnitB, TScalar>
+    where TBaseUnitA : BaseUnit<TQuantityA, TBaseUnitA, TScalar>
+                     , IBaseUnit<TBaseUnitA, TScalar>
+                     , IUnit
+    where TBaseUnitB : BaseUnit<TQuantityB, TBaseUnitB, TScalar>
+                     , IBaseUnit<TBaseUnitB, TScalar>
+                     , IUnit
+    where TScalar : INumber<TScalar>;
+
+
+
+
+// TODO : multiplicative relationship for non-base units
+// TODO : inverse relationship for non-base units
+
+
+
+
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+public class KnownUnit<TQuantity, TUnit, TBaseUnit, TScalar>
+    : Attribute
+    where TQuantity : Quantity<TQuantity, TBaseUnit, TScalar>
+    where TUnit : Quantity<TQuantity, TBaseUnit, TScalar>.Unit<TUnit>
+                , IUnit<TUnit, TBaseUnit, TScalar>
+                , IUnit
+    where TBaseUnit : BaseUnit<TQuantity, TBaseUnit, TScalar>
+                    , IBaseUnit<TBaseUnit, TScalar>
+                    , IUnit
+    where TScalar : INumber<TScalar>;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+public class KnownBaseUnit<TQuantity, TBaseUnit, TScalar>
+    : KnownUnit<TQuantity, TBaseUnit, TBaseUnit, TScalar>
+    where TQuantity : Quantity<TQuantity, TBaseUnit, TScalar>
+    where TBaseUnit : BaseUnit<TQuantity, TBaseUnit, TScalar>
+                    , IBaseUnit<TBaseUnit, TScalar>
+                    , IUnit
+    where TScalar : INumber<TScalar>;
