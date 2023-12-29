0a23ef05994fa2f78cfc0dd70c6b99957e893c68
Unknown6656 <unknown84@gmx.de>
2023-12-29 11:55:22 +0100

    added demo program for internal testing and playing around

---
+++ Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Unknown6656.Units.Euclidean;
+using Unknown6656.Units.Temporal;
+using Unknown6656.Units.Movement;
+using Unknown6656.Units;
+using System.Diagnostics;
+
+
+
+var a = 9.81.MeterPerSecondSquared();
+var m = 400d.Kilogram();
+
+
+Debugger.Break();
