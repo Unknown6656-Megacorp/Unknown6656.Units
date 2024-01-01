5e7da8eafa731d139fec24b37d094636eaf58a7f
Unknown6656 <unknown84@gmx.de>
2024-01-01 14:04:16 +0100

    added more units for power

--- Unknown6656.Units/Energy/Power.cs
+++ Unknown6656.Units/Energy/Power.cs
@@ -9,14 +9,50 @@ public partial record Watt(Scalar Value)
     public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
 }
 
-[KnownUnit<Power, KilowattHour, Watt, Scalar>]
-public partial record KilowattHour(Scalar Value)
-    : Power.AffineUnit<KilowattHour>(Value)
+[KnownUnit<Power, MetricHorsepower, Watt, Scalar>]
+public partial record MetricHorsepower(Scalar Value)
+    : Power.AffineUnit<MetricHorsepower>(Value)
     , ILinearUnit<Scalar>
-    , IUnit<KilowattHour, Watt, Scalar>
-    , IUnit
 {
-    public static string UnitSymbol { get; } = "kWh";
-    public static UnitDisplay UnitDisplay { get; } = Unit.MetricSI_Shifted_k;
-    public static Scalar ScalingFactor { get; } = (Scalar)2.777777777777778e-7;
+    public static string UnitSymbol { get; } = "hp";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
+    public static Scalar ScalingFactor { get; } = (Scalar)0.0013596216;
 }
+
+[KnownUnit<Power, BoilerHorsepower, Watt, Scalar>]
+public partial record BoilerHorsepower(Scalar Value)
+    : Power.AffineUnit<BoilerHorsepower>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "hp";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)0.0001019419950048422447627300066262296753147459095774504307049288;
+}
+
+[KnownUnit<Power, UKHorsepower, Watt, Scalar>]
+public partial record UKHorsepower(Scalar Value)
+    : Power.AffineUnit<UKHorsepower>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "hp";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)0.001341022089595027;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
