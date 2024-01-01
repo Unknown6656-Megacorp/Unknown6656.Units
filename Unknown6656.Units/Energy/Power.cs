6ee317a5ba6a1e9c73f6f6a1128601297a15acc7
Unknown6656 <unknown84@gmx.de>
2024-01-01 13:17:18 +0100

    added power/watt

---
+++ Unknown6656.Units/Energy/Power.cs
@@ -0,0 +1,22 @@
+﻿namespace Unknown6656.Units.Energy;
+
+
+[KnownBaseUnit<Power, Watt, Scalar>]
+public partial record Watt(Scalar Value)
+    : BaseUnit<Power, Watt, Scalar>(Value)
+{
+    public static string UnitSymbol { get; } = "W";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
+}
+
+[KnownUnit<Power, KilowattHour, Watt, Scalar>]
+public partial record KilowattHour(Scalar Value)
+    : Power.AffineUnit<KilowattHour>(Value)
+    , ILinearUnit<Scalar>
+    , IUnit<KilowattHour, Watt, Scalar>
+    , IUnit
+{
+    public static string UnitSymbol { get; } = "kWh";
+    public static UnitDisplay UnitDisplay { get; } = Unit.MetricSI_Shifted_k;
+    public static Scalar ScalingFactor { get; } = (Scalar)2.777777777777778e-7;
+}
--- Unknown6656.Units/Energy/Quantities.cs
+++ Unknown6656.Units/Energy/Quantities.cs
@@ -1,4 +1,9 @@
-﻿namespace Unknown6656.Units.Energy;
+using Unknown6656.Units.Euclidean;
+using Unknown6656.Units.Matter;
+using Unknown6656.Units.Movement;
+using Unknown6656.Units.Temporal;
+
+namespace Unknown6656.Units.Energy;
 
 
 public partial record Temperature(Kelvin value)
@@ -7,5 +12,13 @@ public partial record Temperature(Kelvin value)
 
 }
 
+// TODO : implement shit like  E = 1/2 * m * v^2
+// TODO : implement gravitational energy    E = - g1 * g2 * m1 * m2 / r     or    E = m * g * h
+[MultiplicativeQuantityRelationship<Force, Length, KineticEnergy, Newton, Meter, Joule, Scalar>]
+[MultiplicativeQuantityRelationship<Pressure, Volume, KineticEnergy, Pascal, CubicMeter, Joule, Scalar>]
 public partial record KineticEnergy(Joule value) : Quantity<KineticEnergy, Joule, Scalar>(value);
 
+// TODO : W = kg * (m/s)^2 / s
+// TODO : W = A^2 * Ω
+[MultiplicativeQuantityRelationship<Power, Time, KineticEnergy, Watt, Second, Joule, Scalar>]
+public partial record Power(Watt value) : Quantity<Power, Watt, Scalar>(value);
