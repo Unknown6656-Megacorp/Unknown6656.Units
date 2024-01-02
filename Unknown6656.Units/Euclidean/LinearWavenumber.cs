f63216f5e070c53a2f0af11996cff2065dba22d7
Unknown6656 <unknown84@gmx.de>
2024-01-02 12:40:22 +0100

    added linear wavenumbers

---
+++ Unknown6656.Units/Euclidean/LinearWavenumber.cs
@@ -0,0 +1,20 @@
+﻿namespace Unknown6656.Units.Euclidean;
+
+
+[KnownBaseUnit<LinearWavenumber, ReciprocalMeter, Scalar>]
+public partial record ReciprocalMeter(Scalar Value)
+    : BaseUnit<LinearWavenumber, ReciprocalMeter, Scalar>(Value)
+{
+    public static string UnitSymbol { get; } = "1/{0}m";
+    public static UnitDisplay UnitDisplay { get; } = Unit.MetricSI_InverseFormatted;
+}
+
+[KnownUnit<LinearWavenumber, Kayser, ReciprocalMeter, Scalar>]
+public partial record Kayser(Scalar Value)
+    : LinearWavenumber.AffineUnit<Kayser>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "cm¯¹";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricNonSI;
+    public static double ScalingFactor { get; } = (Scalar)1e-2;
+}
--- Unknown6656.Units/Euclidean/Quantities.cs
+++ Unknown6656.Units/Euclidean/Quantities.cs
@@ -1,7 +1,14 @@
 ﻿namespace Unknown6656.Units.Euclidean;
 
 
-public partial record Angle(Radian value) : Quantity<Angle, Radian, Scalar>(value);
+public partial record Angle(Radian value)
+    : Quantity<Angle, Radian, Scalar>(value)
+{
+    public static Degree North { get; } = new((Scalar)0);
+    public static Degree East { get; } = new((Scalar)90);
+    public static Degree South { get; } = new((Scalar)180);
+    public static Degree West { get; } = new((Scalar)270);
+}
 
 [MultiplicativeQuantityRelationship<Length, Area, Meter, SquareMeter, Scalar>]
 public partial record Length(Meter value) : Quantity<Length, Meter, Scalar>(value);
@@ -10,3 +17,11 @@ public partial record Length(Meter value) : Quantity<Length, Meter, Scalar>(valu
 public partial record Area(SquareMeter value) : Quantity<Area, SquareMeter, Scalar>(value);
 
 public partial record Volume(CubicMeter value) : Quantity<Volume, CubicMeter, Scalar>(value);
+
+[InverseQuantityRelationship<LinearWavenumber, Length, ReciprocalMeter, Meter, Scalar>]
+public partial record LinearWavenumber(ReciprocalMeter value) : Quantity<LinearWavenumber, ReciprocalMeter, Scalar>(value);
+
+// TODO : angular wavenumber
+// TODO : angular wavelength
+// TODO : angular frequency
+// TODO : angular period
