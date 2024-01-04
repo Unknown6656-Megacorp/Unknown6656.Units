53222b698982eeab887bc79a69da64355e76c206
Unknown6656 <unknown84@gmx.de>
2024-01-04 16:09:52 +0100

    added molar mass and g/mol

---
+++ Unknown6656.Units/Matter/MolarMass.cs
@@ -0,0 +1,10 @@
+﻿namespace Unknown6656.Units.Matter;
+
+
+[KnownBaseUnit<MolarMass, GramPerMol, Scalar>]
+public partial record GramPerMol(Scalar Value)
+    : BaseUnit<MolarMass, GramPerMol, Scalar>(Value)
+{
+    public static string UnitSymbol { get; } = "g/mol";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
+}
