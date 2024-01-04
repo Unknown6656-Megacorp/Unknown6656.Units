531be120d6a5d329da3d5e37956eb61723e7d9be
Unknown6656 <unknown84@gmx.de>
2024-01-04 19:07:09 +0100

    added abampere/biot

--- Unknown6656.Units/Electricity/Current.cs
+++ Unknown6656.Units/Electricity/Current.cs
@@ -1,5 +1,6 @@
 ﻿#if !USE_DIACRITICS
 global using Ampère = Unknown6656.Units.Electricity.Ampere;
+global using Abampère = Unknown6656.Units.Electricity.Abampère;
 #endif
 
 namespace Unknown6656.Units.Electricity;
@@ -15,4 +16,28 @@ public partial record Ampere(Scalar Value)
 {
     public static string UnitSymbol { get; } = "A";
     public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
+}
+
+[KnownUnit<Current, Abampère, Ampère, Scalar>]
+#if USE_DIACRITICS
+public partial record Abampère(Scalar Value)
+#else
+public partial record Abampere(Scalar Value)
+#endif
+    : Current.AffineUnit<Abampère>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "abA";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
+    public static Scalar ScalingFactor { get; } = (Scalar).1;
+}
+
+[KnownUnit<Current, Biot, Ampère, Scalar>]
+public partial record Biot(Scalar Value)
+    : Current.AffineUnit<Biot>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "Bi";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI;
+    public static Scalar ScalingFactor { get; } = (Scalar).1;
 }
