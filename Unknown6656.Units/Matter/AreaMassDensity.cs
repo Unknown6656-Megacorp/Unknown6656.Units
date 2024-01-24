b389c512ea8235e9586e14856bc66ce1b0fd5733
Unknown6656 <unknown84@gmx.de>
2024-01-24 17:29:18 +0100

    added lbs/in^2

--- Unknown6656.Units/Matter/AreaMassDensity.cs
+++ Unknown6656.Units/Matter/AreaMassDensity.cs
@@ -8,3 +8,13 @@ public partial record KilogramPerSquareMeter(Scalar Value)
     public static string UnitSymbol { get; } = "kg/m²";
     public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricSI_Shifted_k;
 }
+
+[KnownUnit<AreaMassDensity, PoundPerSquareInch, KilogramPerSquareMeter, Scalar>]
+public partial record PoundPerSquareInch(Scalar Value)
+    : AreaMassDensity.AffineUnit<PoundPerSquareInch>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "lbs/in²";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)0.00014503773773020923;
+}
