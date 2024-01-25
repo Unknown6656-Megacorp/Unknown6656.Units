e6575dc9424be47b64768662195af2e2033c4a37
Unknown6656 <unknown84@gmx.de>
2024-01-25 01:12:44 +0100

    updated test program

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -3,14 +3,19 @@
 using Unknown6656.Units.Euclidean;
 using Unknown6656.Units.Temporal;
 using Unknown6656.Units.Movement;
+using Unknown6656.Units.Matter;
 using Unknown6656.Units;
 
 using System.Diagnostics;
 
 
-
-var a = 9.81.MeterPerSecondSquared();
-var m = 400d.Kilogram();
+Acceleration a = "1 G";
+Mass m = 10.75.Kilogram();
+Force f = m * a;
+Length l = 1d.Meter();
+NewtonMeter t = f * l;
+Speed s = "1 Mach";
+Time tt = s / a;
 
 
 Debugger.Break();
