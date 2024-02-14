0b828e9443365ca399ad9744651eb7eeb829804a
Unknown6656 <unknown84@gmx.de>
2024-02-14 23:17:47 +0100

    added units for luminous intensity

--- Unknown6656.Units/Photometry/LuminousIntensity.cs
+++ Unknown6656.Units/Photometry/LuminousIntensity.cs
@@ -1,2 +1,30 @@
 ﻿namespace Unknown6656.Units.Photometry;
 
+
+[KnownBaseUnit<LuminousIntensity, Candela, Scalar>]
+public partial record Candela(Scalar Value)
+    : BaseUnit<LuminousIntensity, Candela, Scalar>(Value)
+{
+    public static string UnitSymbol { get; } = "cd";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricUseSIPrefixes;
+}
+
+[KnownUnit<LuminousIntensity, CandlePower, Candela, Scalar>]
+public partial record CandlePower(Scalar Value)
+    : LuminousIntensity.AffineUnit<CandlePower>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "cp";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)1.0193679918450560652395514780835881753312945973496432212028542303;
+}
+
+[KnownUnit<LuminousIntensity, HefnerLamp, Candela, Scalar>]
+public partial record HefnerLamp(Scalar Value)
+    : LuminousIntensity.AffineUnit<HefnerLamp>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "HK";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)1.08695652173913;
+}
