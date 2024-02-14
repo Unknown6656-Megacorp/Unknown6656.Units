531f3026e692763815698ef3d69f1206f2c1fd8d
Unknown6656 <unknown84@gmx.de>
2024-02-14 16:41:33 +0100

    added statampere

--- Unknown6656.Units/Electricity/Current.cs
+++ Unknown6656.Units/Electricity/Current.cs
@@ -1,6 +1,7 @@
 ﻿#if !USE_DIACRITICS
 global using Ampère = Unknown6656.Units.Electricity.Ampere;
-global using Abampère = Unknown6656.Units.Electricity.Abampère;
+global using Abampère = Unknown6656.Units.Electricity.Abampere;
+global using Statampère = Unknown6656.Units.Electricity.Statampere;
 #endif
 
 namespace Unknown6656.Units.Electricity;
@@ -30,6 +31,20 @@ public partial record Abampere(Scalar Value)
     public static string UnitSymbol { get; } = "abA";
     public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricUseSIPrefixes;
     public static Scalar ScalingFactor { get; } = (Scalar).1;
+}
+
+[KnownUnit<Current, Statampère, Ampère, Scalar>]
+#if USE_DIACRITICS
+public partial record Statampère(Scalar Value)
+#else
+public partial record Statampere(Scalar Value)
+#endif
+    : Current.AffineUnit<Statampère>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "statA";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricUseSIPrefixes;
+    public static Scalar ScalingFactor { get; } = (Scalar)3.3356409519815204957557671447491851179258151984597290969874e-10;
 }
 
 [KnownUnit<Current, Biot, Ampère, Scalar>]
