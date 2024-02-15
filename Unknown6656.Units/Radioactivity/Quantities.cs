52c03a214188c30ad4c6d7c4aa99d03a45087873
Unknown6656 <unknown84@gmx.de>
2024-02-15 21:34:38 +0100

    began adding unit/quantity for radiation exposure

---
+++ Unknown6656.Units/Radioactivity/Quantities.cs
@@ -0,0 +1,20 @@
+﻿using Unknown6656.Units.Electricity;
+using Unknown6656.Units.Matter;
+
+namespace Unknown6656.Units.Radioactivity;
+
+
+[MultiplicativeRelationship<RadiationExposure, Mass, Charge, Röntgen, Kilogram, Coulomb, Scalar>]
+public partial record RadiationExposure(Röntgen value)
+    : Quantity<RadiationExposure, Röntgen, Scalar>(value)
+{
+    public static string QuantitySymbol { get; } = "X";
+}
+
+
+// activity
+// specific activity
+// absorbed dose
+// equivalent dose
+// kerma https://en.wikipedia.org/wiki/Kerma_(physics)
+
---
+++ Unknown6656.Units/Radioactivity/RadiationExposure.cs
@@ -0,0 +1,18 @@
+﻿#if !USE_DIACRITICS
+global using Röntgen = Unknown6656.Units.Radioactivity.Roentgen;
+#endif
+
+namespace Unknown6656.Units.Radioactivity;
+
+
+[KnownBaseUnit<RadiationExposure, Röntgen, Scalar>]
+#if USE_DIACRITICS
+public partial record Röntgen(Scalar Value)
+#else
+public partial record Roentgen(Scalar Value)
+#endif
+    : BaseUnit<RadiationExposure, Röntgen, Scalar>(Value)
+{
+    public static string UnitSymbol { get; } = "R";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricUseSIPrefixes;
+}
