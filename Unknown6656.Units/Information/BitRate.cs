0fa9336dfe31217ec1b36ee4e9a6aa0c0a5b0b64
Unknown6656 <unknown84@gmx.de>
2024-02-17 16:54:06 +0100

    added B/s

--- Unknown6656.Units/Information/BitRate.cs
+++ Unknown6656.Units/Information/BitRate.cs
@@ -6,6 +6,16 @@ public partial record BitPerSecond(Scalar Value)
     : BaseUnit<BitRate, BitPerSecond, Scalar>(Value)
 {
     public static string UnitSymbol { get; } = "bps";
-    static string[] IUnit.AlternativeUnitSymbols { get; } = ["bit/s"];
+    static string[] IUnit.AlternativeUnitSymbols { get; } = ["bit/s", "bit/sec"];
     public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricUseSIPrefixes;
+}
+
+[KnownUnit<BitRate, BytePerSecond, BitPerSecond, Scalar>]
+public partial record BytePerSecond(Scalar Value)
+    : BaseUnit<BitRate, BytePerSecond, Scalar>(Value)
+{
+    public static string UnitSymbol { get; } = "B/s";
+    static string[] IUnit.AlternativeUnitSymbols { get; } = ["byte/s", "B/secnod", "byte/second", "byte/sec", "B/sec"];
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricUseSIPrefixes;
+    public static Scalar ScalingFactor { get; } = Byte.ScalingFactor;
 }
