e193d865b072116593cd8239fb2272e8140840e7
Unknown6656 <unknown84@gmx.de>
2024-02-17 16:54:06 +0100

    fixed critical error

--- Unknown6656.Units/Information/BitRate.cs
+++ Unknown6656.Units/Information/BitRate.cs
@@ -12,10 +12,11 @@ public partial record BitPerSecond(Scalar Value)
 
 [KnownUnit<BitRate, BytePerSecond, BitPerSecond, Scalar>]
 public partial record BytePerSecond(Scalar Value)
-    : BaseUnit<BitRate, BytePerSecond, Scalar>(Value)
+    : BitRate.AffineUnit<BytePerSecond>(Value)
+    , ILinearUnit<Scalar>
 {
     public static string UnitSymbol { get; } = "B/s";
-    static string[] IUnit.AlternativeUnitSymbols { get; } = ["byte/s", "B/secnod", "byte/second", "byte/sec", "B/sec"];
+    static string[] IUnit.AlternativeUnitSymbols { get; } = ["byte/s", "B/second", "byte/second", "byte/sec", "B/sec"];
     public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricUseSIPrefixes;
     public static Scalar ScalingFactor { get; } = Byte.ScalingFactor;
 }
