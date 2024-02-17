a5e82dc652a9fd323c7a796e3813982576879011
Unknown6656 <unknown84@gmx.de>
2024-02-17 23:47:53 +0100

    added more unconventional units of measure for areas

--- Unknown6656.Units/Euclidean/Area.cs
+++ Unknown6656.Units/Euclidean/Area.cs
@@ -121,3 +121,64 @@ public partial record Barn(Scalar Value)
     public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricUseSIPrefixes;
     public static Scalar ScalingFactor { get; } = (Scalar)1e28;
 }
+
+[KnownUnit<Area, Square, SquareMeter, Scalar>]
+public partial record Square(Scalar Value)
+    : Area.AffineUnit<Square>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "square";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = SquareFoot.ScalingFactor * 100;
+}
+
+[KnownUnit<Area, FIFAFootballField, SquareMeter, Scalar>]
+public partial record FIFAFootballField(Scalar Value)
+    : Area.AffineUnit<FIFAFootballField>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "FIFA field";
+    static string[] IUnit.AlternativeUnitSymbols { get; } = ["field", "field FIFA", "FIFA football pitch", "FIFA pitch", "football field FIFA", "football pitch FIFA"];
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)1.400560224089635854341736694677871148459383753501400560224089e-4;
+}
+
+[KnownUnit<Area, AmericanFootballField, SquareMeter, Scalar>]
+public partial record AmericanFootballField(Scalar Value)
+    : Area.AffineUnit<AmericanFootballField>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "US field";
+    static string[] IUnit.AlternativeUnitSymbols { get; } = [
+        "field US", "US football pitch", "US pitch", "football field US", "football pitch US", "USA field",
+        "field USA", "USA football pitch", "USA pitch", "football field USA", "football pitch USA", "american field",
+        "field american", "american football pitch", "american pitch", "football field american", "football pitch american",
+    ];
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)1.868734447345437900752344714565972341821226852330247870372283e-4;
+}
+
+[KnownUnit<Area, CanadianFootballPitch, SquareMeter, Scalar>]
+public partial record CanadianFootballPitch(Scalar Value)
+    : Area.AffineUnit<CanadianFootballPitch>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "CA field";
+    static string[] IUnit.AlternativeUnitSymbols { get; } = [
+        "field CA", "CA football pitch", "CA pitch", "football field CA", "football pitch CA", "canada field",
+        "field canada", "canada football pitch", "canada pitch", "football field canada", "football pitch canada", "canadaian field",
+        "field canadaian", "canadaian football pitch", "canadaian pitch", "football field canadaian", "football pitch canadaian"
+    ];
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.Imperial;
+    public static Scalar ScalingFactor { get; } = (Scalar)1.672713351470042316757343520730380837434384874813089002850715e-4;
+}
+
+[KnownUnit<Area, Morgen, SquareMeter, Scalar>]
+public partial record Morgen(Scalar Value)
+    : Area.AffineUnit<Morgen>(Value)
+    , ILinearUnit<Scalar>
+{
+    public static string UnitSymbol { get; } = "Mg";
+    public static UnitDisplay UnitDisplay { get; } = UnitDisplay.MetricNoSIPrefixes;
+    public static Scalar ScalingFactor { get; } = (Scalar)1.1674987040764384751532925798452363717876273157336795356157154665e-4;
+}
