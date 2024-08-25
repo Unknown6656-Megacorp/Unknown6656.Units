e8808ea567105e13fe5455e931b44700bf7f087b
Unknown6656 <Unknown6656@users.noreply.github.com>
2024-08-25 23:20:51 +0200

    added placeholder code file

---
+++ Unknown6656.Physics/Chemistry/PeriodicSystemOfElements.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unknown6656.Physics.Chemistry;
+
+
+// TODO : implement the periodic table of elements
